80ae8c126ad71ac2c02162eea888472959845d8c
pansongkui <pansongkui@360.cn>
2018-04-10 17:47:22 +0800

    mock调整

---
+++ src/mock/tree-folder.js
@@ -0,0 +1,26 @@
+export const treeDatas = [
+  {
+    name: 'node-1',
+    deptLevel: 1,
+    children: [
+      {
+        name: 'node-1-a',
+        deptLevel: 2,
+        children: [
+          { name: 'node-1-a-1', deptLevel: 3 },
+          { name: 'node-1-a-2', deptLevel: 3 }
+        ]
+      }
+    ]
+  },
+  {
+    name: 'node-2',
+    deptLevel: 1,
+    children: [
+      {
+        name: 'node-2-a',
+        deptLevel: 2
+      }
+    ]
+  }
+]
---
+++ src/views/list.vue
@@ -0,0 +1,29 @@
+ <template>
+  <div id="app">
+    this is a folder tree.
+    <tree-folder
+      v-for="tree in treeDatas"
+      :tree="tree"
+      :treeProps="treeProps"
+      :key="tree[treeProps.label]"/>
+  </div>
+</template>
+
+<script>
+import TreeFolder from '@/components/tree-folder'
+import { treeDatas } from '@/mock/tree-folder'
+export default {
+  name: 'app',
+  components: { TreeFolder },
+  data () {
+    return {
+      treeProps: {
+        label: 'name',
+        children: 'children',
+        level: 'deptLevel'
+      },
+      treeDatas: treeDatas
+    }
+  }
+}
+</script>
